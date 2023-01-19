c037e978c5fe72374b034de9cba398902877a1e0
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-19 21:41:37 +0545

    function created with parameters

--- Function/Ex1.js
+++ Function/Ex1.js
@@ -1,7 +1,15 @@
 //Function Declaration without parameters
 function school_detail(){
-    console.log("Here is my school details")
+    console.log("This is the result from function without parameters.")
 }
 
 //Function Calling Without parameters
 school_detail()
+
+
+//Function With Parameters
+function take_param(person1,person2){
+    console.log("This is the result from function with parameters where Person1 is " +person1 +" and Person2 is "+person2 +".")
+}
+//Function calling with parameters
+take_param("Ram", "Sita")
