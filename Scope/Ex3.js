eef746c0b4efd6ee67a61b4c7b5acb060d489f4b
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-21 21:29:53 +0545

    IIFE module

---
+++ Scope/Ex3.js
@@ -0,0 +1,14 @@
+// IIFE ( Immediately Invoked function expression)
+
+
+// (function (){ // This is a function expression rather than funtion declaration
+//     var a = 20 ;
+//     console.log("The value of a is", a)
+// })()
+
+// console.log("The Value of a outside the function expression is", a)  // This line creates an reference error: a is not defined 
+
+
+function one (){
+    var x = 2
+}
