add7f4a61234afc38382b995e57d71270a80784a
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-21 22:33:31 +0545

    implementation of this keyword

--- This_Keyword/Ex1.js
+++ This_Keyword/Ex1.js
@@ -1,10 +1,34 @@
 //'this' keyword is the object that the function is property of
 
-const obj ={
-    name : "Billi",
-    sing: function(){
-        return "Lalala "+ this.name  //here this refers to obj ; this keywordis similar to self keyword in python
-    }
-}
 
-console.log(obj.sing())
+//1. gives method access to their objects
+// const obj ={
+//     name : "Billi",
+//     sing: function(){
+//         return "Lalala "+ this.name  //here this refers to obj ; this keywordis similar to self keyword in python
+//     },
+//     sing_again(){
+//         return this.sing() + '!' // we can use the sing()function here
+//     }
+// }
+
+// console.log(obj.sing())
+// console.log(obj.sing_again())
+
+
+//2. Executes same code for different objects
+function importantPerson(){
+    return this.name
+}
+const name ='Harry' ;
+const obj1 ={
+    name: 'Joseph',
+    important_person :importantPerson
+}
+const obj2 ={
+    name: 'Mith',
+    important_person :importantPerson
+}
+console.log(importantPerson())  // This should have return Harry but not giving result undefined
+console.log(`I am `, obj2.important_person()) //  I am  Mith
+console.log(`I am ` ,obj1.important_person()) //   I am  Joseph
