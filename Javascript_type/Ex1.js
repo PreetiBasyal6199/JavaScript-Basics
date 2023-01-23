ebd37cc1869aa62cec67eb1be8f9d2768c04adbf
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-23 22:35:43 +0545

    declared the types in js

---
+++ Javascript_type/Ex1.js
@@ -0,0 +1,31 @@
+/*Types in Javascript
+
+  Primitive Data Types
+
+1. number (5)
+2. boolean (true)
+3. string("Hello")
+4. undefined
+5.null
+ 
+Non Primitives Data Types
+
+6. symbol Symbol('just me'))
+7. {}
+8. function function m (){}
+
+*/
+
+
+console.log(typeof(5)) // number
+console.log(typeof(true)) //boolean
+console.log(typeof("Hello There")) // string
+console.log(typeof(undefined))// undefined : absence of defination
+console.log(typeof(null))// object : absence of value
+console.log(typeof Symbol("just you")) //symbol
+console.log(typeof {}) // object
+
+/*
+   All data types except "object" are primitive data types. Primitive data type are the data 
+   that only represents a single value.
+*/
