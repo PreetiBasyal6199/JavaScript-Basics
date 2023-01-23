f30868609bd423c5c14eeb65b33200f082b4c0f0
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-23 23:33:54 +0545

    implementation of spread operator

--- Javascript_type/Ex2.js
+++ Javascript_type/Ex2.js
@@ -22,3 +22,42 @@ obj2.password = "password2"
 
 console.log(obj1.password)     // password2
 console.log(obj2.password)     // password2
+
+
+// The solution for the above problem
+
+
+let obj3 ={
+    user: "user3",
+    password: "password3"
+}
+
+let obj4 = Object.assign({}, obj3)
+
+let obj5 = {...obj3}     //spread operator
+
+obj4.password ="password4"
+obj5.password = "password5"
+console.log(obj3.password)     // password3
+console.log(obj4.password)     // password4
+console.log(obj5.password)     // password4
+
+
+
+
+// Another Example of passby reference
+
+var c =[1,2,3,4,5]
+var d = c
+d.push(123456)
+console.log(c)                  // [1,2,3,4,5,123456]
+console.log(d)                  // [1,2,3,4,5,123456]
+
+//Solution for the above problem
+
+var c =[1,2,3,4,5]
+var d = [].concat(c)
+d.push(123456)
+console.log(c)                  // [1,2,3,4,5]
+console.log(d)                  // [1,2,3,4,5,123456]
+
