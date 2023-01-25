5c9768b410bbb640f9fade48b1f8e78dd2ef8fad
Preeti Basyal <preetibasyal412@gmail.com>
2023-01-25 21:00:44 +0545

    Type Coercion defination

---
+++ Javascript_type/Ex4.js
@@ -0,0 +1,18 @@
+/*Type Coercion:  Type Coercion refers to the process of automatic or implicit conversion 
+    of values from one data type to another.
+
+*/
+
+console.log(1 == "1") ;  // true
+console.log(1==="1")   ;   //false
+
+
+if (1){
+    console.log("Hello There")    // Hello There;   Here 1 is equals to true
+}
+
+if (0){
+    console.log("Hello There")    // But it returns nothing
+}
+
+console.log(1=="true")
