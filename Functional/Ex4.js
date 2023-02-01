c77a51e6696392c284ad4f616eee4e869a4d483e
PreetiBasyal6199 <preetibasyal412@gmail.com>
2023-02-01 17:03:16 +0545

    Recursive function

---
+++ Functional/Ex4.js
@@ -0,0 +1,27 @@
+// Recursion
+
+
+let function1 = function get_list(num){
+    if (num==0){return ;}
+    console.log(num)
+    function1(num - 1)
+        
+    }
+function1(10)
+
+/*
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+*/
+    
+
+
