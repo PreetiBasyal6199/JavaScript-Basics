ff78a945a01ab0dbd52bc7af811699fa62ca0896
PreetiBasyal6199 <preetibasyal412@gmail.com>
2023-02-02 17:09:31 +0545

    added functionalities to the object

--- This_Keyword/Ex1.js
+++ This_Keyword/Ex1.js
@@ -14,6 +14,11 @@ const obj ={
 
 console.log(obj.sing())
 console.log(obj.sing_again())
+console.log(`The keys of the object are` ,Object.keys(obj))    // The keys of the object are [ 'name', 'sing', 'sing_again' ]
+console.log(`The values of the object are` ,Object.values(obj))    // The values of the object are [ 'Billi', [Function: sing], [Function: sing_again] ]
+console.log(Object.entries(obj))         //[[ 'name', 'Billi' ],[ 'sing', [Function: sing] ],[ 'sing_again', [Function: sing_again] ]]
+console.log(obj.hasOwnProperty('name'))     // true   i.e the key 'name' exists in the object.
+console.log(obj.hasOwnProperty('address'))     // false  i.e. the key  address does not exists in the object.
 
 
 //2. Executes same code for different objects
