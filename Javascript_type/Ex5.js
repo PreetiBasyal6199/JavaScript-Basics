98e1ce36f5c7eb33898c49ea2b9e9fbf99c79b76
PreetiBasyal6199 <preetibasyal412@gmail.com>
2023-02-02 15:03:46 +0545

    Math Objects examples

---
+++ Javascript_type/Ex5.js
@@ -0,0 +1,29 @@
+console.log(Math.PI)            //gives the valuw of pi in math
+
+console.log(Math.round(Math.PI))    //3    gives the round value of pi
+
+console.log(Math.floor(Math.PI))     //3    gives the lowest round off value
+
+console.log(Math.ceil(Math.PI))        //4  gives the highest round off value
+
+console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
+
+console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
+
+console.log(Math.random())                  // returns random value between 0 and 0.999...
+
+console.log(Math.random() * 11)               //returns random value between 0 and 10
+
+console.log(Math.abs(-69))                      //69
+
+console.log(Math.sqrt(25))                      //5
+
+console.log(Math.pow(2,3))                      //8
+
+// Returns the natural logarithm with base E of x, Math.log(x)
+console.log(Math.log(2))        // 0.6931471805599453
+console.log(Math.log(10))       // 2.302585092994046
+
+// Returns the natural logarithm of 2 and 10 respectively
+console.log(Math.LN2)           // 0.6931471805599453
+console.log(Math.LN10)          // 2.302585092994046
