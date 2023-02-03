219b627696accc3b94a76d6b2f3cbca6c2d32cdc
Preeti Basyal <preetibasyal412@gmail.com>
2023-02-03 15:37:47 +0545

    Try, catch and finally

---
+++ Error Handling/Ex1.js
@@ -0,0 +1,14 @@
+
+try{
+    a=4
+    c=a/b ;
+    console.log("The result is ", c)
+}
+catch(error){
+    console.log("Oops, there is an error")      //Catch block is executed if any error occurs
+}
+
+finally{
+    console.log("Hey you got the solution")       // This block of code is always executed
+}
+
