db6fa3ca0a72a91bb5806252babbfe4d87eb69bb
Preeti Basyal <preetibasyal412@gmail.com>
2023-02-04 13:43:16 +0545

    Example for spread operator

--- Async/Ex2.js
+++ Async/Ex2.js
@@ -1,5 +1,7 @@
 // Object Spread Operator
 
+
+//Example1
 const animals ={
     tiger: 20,
     lion : 10,
@@ -13,3 +15,20 @@ console.log(tiger)          //20
 console.log(lion)           //10
 console.log(rest)           //{bird:40, monkey:2}
 
+const getAnimals = function(p1,p2,p3){
+    console.log(p1)             //20
+    console.log(p2)             //10
+    console.log(p3)             //{bird:40, monkey:2}
+
+}
+getAnimals(tiger, lion, rest)
+
+// Example2
+
+const array1= [1,2,3,4,5]
+
+function addResult(a,b,c,d,e){
+    return a+b+c+d+e        
+}
+
+console.log(addResult(...array1))           // 15 ; It is same as addResult(1,2,3,4,5)
