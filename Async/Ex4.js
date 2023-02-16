76b388c9bce4c174b6e96c89032ddcca3ce51d27
Preeti Basyal <preetibasyal412@gmail.com>
2023-02-16 20:56:58 +0545

    Async function

--- Async/Ex4.js
+++ Async/Ex4.js
@@ -1,7 +1,25 @@
-const createTodo = async ()=>{
+const getTodo = async ()=>{
    let response = await fetch('https://jsonplaceholder.typicode.com/users')  // fetch for get request
     let resp = await response.json()
     return resp
+}
+
+const createTodo = async () =>{
+    response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+    method: 'POST',
+    headers:{
+        'Content-type': 'application/json'
+    },
+    body:JSON.stringify({
+        'userId': 1022,
+        'id':102,
+        'title': "Preetis Post",
+        "body":"Hello"
+    })
+    }
+    
+
+    )
 }
 
 const mainFunc = async() =>{
@@ -10,3 +28,4 @@ const mainFunc = async() =>{
 }
 
 mainFunc()
+
